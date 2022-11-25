6eb619981c5ad51c8895fbb7825339a2b994511d
Sajid Ali <sajidali.gcu@gmail.com>
2022-11-25 12:42:53 +0500

    added a custom error and remove try catch from async methods by creating a higher order function'

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const AppError = require("./utils/appError");
+const globalErrorHnadler = require("./controller/errorController");
 const app = express();
 
 dotenv.config({ path: "./config.env" });
@@ -19,5 +21,11 @@ app.use(express.json());
 
 //Routes
 app.use("/api/users", userRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHnadler);
 
 module.exports = app;
